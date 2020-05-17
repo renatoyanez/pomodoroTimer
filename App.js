e7587bdd8ed4df911232033e3404fd195c2d3fa4
Tato <renatoyanez95@gmail.com>
2020-05-17 02:29:33 -0300

    first commit, timer initialised

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, StatusBar, TouchableOpacity, Dimensions } from 'react-native';
+import { vibrate } from './utils'
+
+const screen = Dimensions.get('window');
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle='light-content' />
+      <TouchableOpacity style={styles.button} onPress={() => null} >
+        <Text style={styles.buttonText}>Start</Text>
+      </TouchableOpacity>
+      <Text style={styles.timerText}>00:00</Text>
     </View>
   );
 }
@@ -12,8 +20,114 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#07121B',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    borderWidth: 5,
+    borderColor: 'green',
+    width: screen.width / 3,
+    height: screen.width / 3,
+    borderRadius: screen.width / 3,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 45,
+    color: 'green'
+  },
+  timerText: {
+    color: '#fff',
+    fontSize: 90,
+    marginBottom: 20
+  }
 });
+
+// import React, { useState, useEffect } from 'react';
+// import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Dimensions } from 'react-native';
+
+// const screen = Dimensions.get('window');
+
+// const formatNumber = number => `0${number}`.slice(-2);
+
+// const getRemaining = (time) => {
+//     const mins = Math.floor(time / 60);
+//     const secs = time - mins * 60;
+//     return { mins: formatNumber(mins), secs: formatNumber(secs) };
+// }
+
+// export default function App() {
+//   const [remainingSecs, setRemainingSecs] = useState(0);
+//   const [isActive, setIsActive] = useState(false);
+//   const { mins, secs } = getRemaining(remainingSecs);
+
+//   toggle = () => {
+//     setIsActive(!isActive);
+//   }
+
+//   reset = () => {
+//     setRemainingSecs(0);
+//     setIsActive(false);
+//   }
+
+//   useEffect(() => {
+//     let interval = null;
+//     if (isActive) {
+//       interval = setInterval(() => {
+//         setRemainingSecs(remainingSecs => remainingSecs + 1);
+//       }, 1000);
+//     } else if (!isActive && remainingSecs !== 0) {
+//       clearInterval(interval);
+//     }
+//     return () => clearInterval(interval);
+//   }, [isActive, remainingSecs]);
+
+
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar barStyle="light-content" />
+//       <Text style={styles.timerText}>{`${mins}:${secs}`}</Text>
+//       <TouchableOpacity onPress={toggle} style={styles.button}>
+//           <Text style={styles.buttonText}>{isActive ? 'Pause' : 'Start'}</Text>
+//       </TouchableOpacity>
+//       <TouchableOpacity onPress={reset} style={[styles.button, styles.buttonReset]}>
+//           <Text style={[styles.buttonText, styles.buttonTextReset]}>Reset</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#07121B',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   button: {
+//       borderWidth: 10,
+//       borderColor: '#B9AAFF',
+//       width: screen.width / 2,
+//       height: screen.width / 2,
+//       borderRadius: screen.width / 2,
+//       alignItems: 'center',
+//       justifyContent: 'center'
+//   },
+//   buttonText: {
+//       fontSize: 45,
+//       color: '#B9AAFF'
+//   },
+//   timerText: {
+//       color: '#fff',
+//       fontSize: 90,
+//       marginBottom: 20
+//   },
+//   buttonReset: {
+//       marginTop: 20,
+//       borderColor: "#FF851B"
+//   },
+//   buttonTextReset: {
+//     color: "#FF851B"
+//   }
+// });
